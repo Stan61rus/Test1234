db6be28cf42b9470d1c4eefa38afea0e9367e7b4
Stan61rus <37905051+Stan61rus@users.noreply.github.com>
2021-05-05 10:24:54 +0300

    Initial commit

---
+++ qapi.js
@@ -0,0 +1,29 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"609103f6765ed9001e2a6b09",
+				"609103f6765ed9001e2a6b0b"
+			]
+		},
+		"609103f6765ed9001e2a6b09": {
+			"id": "609103f6765ed9001e2a6b09",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"609103f6765ed9001e2a6b0b": {
+			"id": "609103f6765ed9001e2a6b0b",
+			"name": "index",
+			"pageUrl": "index"
+		}
+	},
+	"mode": "production",
+	"projectType": "gatsby",
+	"site": {
+		"styles": {},
+		"seo": {}
+	}
+}
